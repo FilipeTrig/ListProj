88ef1e422e4106f82c49fc631794f3ad1d8f16bd
FilipeTrig <fliptrig@gmail.com>
2025-02-24 10:49:55 +0000

    changes to JSON handling

--- ListProj/src/main/java/ListProj/data/JSONData.java
+++ ListProj/src/main/java/ListProj/data/JSONData.java
@@ -1,18 +1,108 @@
 package ListProj.data;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.KeyStore.Entry;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.boot.CommandLineRunner;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import ListProj.models.EntryModel;
 
 public class JSONData {
+
+    private ObjectMapper objectMapper;
+   
+    public ArrayList<Boolean> readJSON(ArrayList<Boolean> items) throws JsonMappingException, JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ArrayList<Boolean> resuList = objectMapper.readerFor(Boolean.class).readValue(items.toString());
+        //ArrayList<Boolean> resuList = new ArrayList<Boolean>();
+        //for (int i = 0; i < items.size(); i++) {
+            //resuList.add(items.get(i));
+        //}
+        return resuList;
+    }
+
+
+    // write items in JSON format so Entry can be stored in database
+    public ArrayList<Boolean> writeJSON(ArrayList<Boolean> items) throws JsonMappingException, JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ArrayList<Boolean> resuList = objectMapper.readValue(items.toString(), new TypeReference<ArrayList<Boolean>>(){});
+        return resuList;
+    }
+
+
+    //@Override
+    public void convert(String... args) throws Exception {
+        ArrayList<EntryModel> entries= new ArrayList();
+        JsonNode json;
+        //need to add the entry as a streamn from the database
+        /*
+        try (InputStream inputStream = TypeReference.class.getResourceAsStream("")) {
+            json = objectMapper.readValue(inputStream, JsonNode.class);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read JSON data", e);
+        }
+        */
+
+        for (int i=0;args.length > i;i++) {
+            json = objectMapper.readTree(args[i]);
+            if (json.isArray()) {
+                for (JsonNode node : json) {
+                    entries.add(createEntryFromNode(node));
+                }
+            } else {
+                entries.add(createEntryFromNode(json));
+            }
+        }
+
+        //throw new UnsupportedOperationException("Unimplemented method 'run'");
+
+
+    }
+
+    private EntryModel createEntryFromNode(JsonNode entry) {
+        //JsonNode node = entry.get("node");
+        String id = entry.get("id").asText();
+        String date = entry.get("date").asText();
+        String weight = entry.get("weight").asText();
+        String items = entry.get("items").asText();
+
+        return NewEntry(date, id, items, weight);
+    }
+
+    private ArrayList extractItems(String node) {
+        ArrayList returnItems = new ArrayList();
+
+        String[] items=node.split(",");
+
+        //JsonNode items = node.get("items");
+        for (int i = 0; i < items.length; i++) {
+            returnItems.add(Boolean.parseBoolean(items[i]));            
+        }
+        // must change to return a ArrayLisy of bolleans
+        return returnItems;
+    }
+
+    private EntryModel NewEntry(String date,String id,String weight, String items){
+        EntryModel entry= new EntryModel();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        entry.setDate(LocalDate.parse(date,formatter));
+        entry.setWeight(Integer.parseInt(weight));
+        entry.setItems(extractItems(items)); // need to change method above to return an array of booleans
+        return entry;
+    }
+
     /*
     public String convertItemsJSON(ArrayList<Boolean> items) { //converts the items to JSON
         ArrayList<Boolean> returnItems = new ArrayList<Boolean>();
@@ -51,24 +141,6 @@ public class JSONData {
     }
 
     */
-
-    public ArrayList<Boolean> readJSON(ArrayList<Boolean> items) throws JsonMappingException, JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        ArrayList<Boolean> resuList = objectMapper.readerFor(Boolean.class).readValue(items.toString());
-        //ArrayList<Boolean> resuList = new ArrayList<Boolean>();
-        //for (int i = 0; i < items.size(); i++) {
-            //resuList.add(items.get(i));
-        //}
-        return resuList;
-    }
-
-
-    // write items in JSON format so Entry can be stored in database
-    public ArrayList<Boolean> writeJSON(ArrayList<Boolean> items) throws JsonMappingException, JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        ArrayList<Boolean> resuList = objectMapper.readValue(items.toString(), new TypeReference<ArrayList<Boolean>>(){});
-        return resuList;
-    }
 
      /*
      * public class Main {  
--- ListProj/src/main/java/ListProj/models/EntryMapper.java
+++ ListProj/src/main/java/ListProj/models/EntryMapper.java
@@ -10,21 +10,27 @@ import java.util.ArrayList;
 import org.springframework.jdbc.core.RowMapper;
 
 
-public class EntryMapper implements RowMapper<EntryJSONModel> {
+public class EntryMapper implements RowMapper<EntryModel> {
 
     @Override
-    public EntryJSONModel mapRow(ResultSet rs, int rowNum) throws SQLException {
-        /*/
+    public EntryModel mapRow(ResultSet rs, int rowNum) throws SQLException {
+        
         EntryModel entry = new EntryModel();        
         ResultSetMetaData rsmd = rs.getMetaData(); 
         int columnCount = rsmd.getColumnCount();
-        ArrayList<Boolean> items = new ArrayList<Boolean>(columnCount);
+        // column count can't be the number of items needs changing for number of items
+        // number of columsn will be thr parameters of each Entry
+        ArrayList<Boolean> items = new ArrayList<Boolean>(54);
         entry.setDate(rs.getDate("date").toLocalDate());
-        for (int i = 1; i <= columnCount; i++) {
+        entry.setId(rs.getInt("ID"));
+        entry.setWeight(rs.getInt("weight"));
+        for (int i = 1; i <= 55; i++) {
             items.add(rs.getBoolean("item" + i));
         }
         entry.setItems(items);
-        */
+        return entry;
+        
+        /*
         EntryJSONModel entryJSON = new EntryJSONModel(); 
         EntryModel entry = new EntryModel(); 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
@@ -39,5 +45,6 @@ public class EntryMapper implements RowMapper<EntryJSONModel> {
         entryJSON.setId(rs.getInt("id"));
         System.out.println(entryJSON.toString());
         return entryJSON;
+        */
     }
 }
